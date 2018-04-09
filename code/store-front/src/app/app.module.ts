28369a2b8cc32f2d672a2ec80718b6cbb16b30d3
Patrick <patpang71@gmail.com>
2018-04-08 17:39:31 -0700

    Add store front to git

---
+++ code/store-front/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import 'hammerjs';
+
+import { LoginService } from './services/login.service';
+import { UserService } from './services/user.service';
+import { PaymentService } from './services/payment.service';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MyAccountComponent } from './components/my-account/my-account.component';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    MyAccountComponent,
+    MyProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MatButtonModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    routing
+  ],
+  providers: [
+    LoginService, 
+    UserService,
+    PaymentService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
